df4bfc6b337cd50eef6ce1758aa1aa9c256b0226
m_thu <mthw.410@gmail.com>
2026-03-31 02:10:29 +0700

    update

--- includes/account_sidebar.php
+++ includes/account_sidebar.php
@@ -112,7 +112,6 @@
             border-radius: 8px;
             background-color: #fff;
             outline: none;
-            /* Tạo mũi tên dropdown tùy chỉnh */
             appearance: none;
             background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='%23141718' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3e%3cpolyline points='6 9 12 15 18 9'%3e%3c/polyline%3e%3c/svg%3e");
             background-repeat: no-repeat;
